91853fb3f5956795d8b68581949b30eb942d75b9
Jorge Bolivar <jlbolivarz@gmail.com>
2023-07-21 20:28:34 -0500

    Innit Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Autofinanzas
+
+A brief description of what this project does and who it's for
+
---
+++ src/main/java/com/jbolivar/autofinanzas/AutofinanzasApplication.java
@@ -0,0 +1,13 @@
+package com.jbolivar.autofinanzas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutofinanzasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutofinanzasApplication.class, args);
+	}
+
+}
