e6d948fe658ebd33fce9ff46c7f2aae66cace408
jorge <jlbolivarz@gmail.com>
2023-09-13 17:40:05 -0500

    Controladores y Repositorys Test unitarios de controladores
    Signed-off-by: jorge <jlbolivarz@gmail.com>

--- src/main/java/com/jbolivar/autofinanzas/controller/CategoriaController.java
+++ src/main/java/com/jbolivar/autofinanzas/controller/CategoriaController.java
@@ -2,6 +2,7 @@ package com.jbolivar.autofinanzas.controller;
 
 import com.jbolivar.autofinanzas.models.Categoria;
 import com.jbolivar.autofinanzas.services.CategoriaService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -16,26 +17,31 @@ public class CategoriaController {
     }
 
     @GetMapping("/")
+    @PreAuthorize(value = "hasRole('USER,ADMIN')")
     public Flux<Categoria> getCategoria(){
         return categoriaService.findAll();
     }
 
     @GetMapping("/{nombre}")
+    @PreAuthorize(value = "hasRole('USER,ROLE_ADMIN')")
     public Flux<Categoria> getCategoriaByNombre(@PathVariable String nombre){
         return categoriaService.findByNombre(nombre);
     }
 
     @PostMapping("/")
+    @PreAuthorize("hasRole('ADMIN')")
     public Mono<Categoria> createCategoria(@RequestBody Categoria categoria){
         return categoriaService.save(categoria);
     }
 
     @PutMapping("/")
+    @PreAuthorize("hasRole('ADMIN')")
     public Mono<Categoria> updateCategoria(@RequestBody Categoria categoria){
         return categoriaService.save(categoria);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public Mono<Categoria> deleteCategoria(@PathVariable Integer id){
         return categoriaService.deleteById(id);
     }
--- src/main/java/com/jbolivar/autofinanzas/controller/UsuarioController.java
+++ src/main/java/com/jbolivar/autofinanzas/controller/UsuarioController.java
@@ -1,19 +1,29 @@
 package com.jbolivar.autofinanzas.controller;
 
 import com.jbolivar.autofinanzas.models.Usuario;
+import com.jbolivar.autofinanzas.services.UsuarioConsumerService;
+import com.jbolivar.autofinanzas.services.UsuarioSQSService;
 import com.jbolivar.autofinanzas.services.UsuarioService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
     UsuarioService usuarioService;
+    UsuarioConsumerService usuarioConsumerService;
+    UsuarioSQSService usuarioSQSService;
 
-    public UsuarioController(UsuarioService usuarioService) {
+    public UsuarioController(UsuarioService usuarioService, UsuarioConsumerService usuarioConsumerService, UsuarioSQSService usuarioSQSService) {
         this.usuarioService = usuarioService;
+        this.usuarioConsumerService = usuarioConsumerService;
+        this.usuarioSQSService = usuarioSQSService;
     }
+
     @GetMapping("/")
     public Flux<Usuario> getUsuarios(){
         return usuarioService.findAll();
@@ -45,5 +55,15 @@ public class UsuarioController {
     @DeleteMapping("/all")
     public Mono<Void> deleteAllUsuario(){
         return usuarioService.deleteAll();
+    }
+
+    @GetMapping("/topico-kafka/{topico}")
+    public Mono<String> getUsuarioFromTopicoKafka(@PathVariable String topico){
+        return  Mono.just(usuarioConsumerService.obtenerNombreUltimoUsuario(topico));
+    }
+
+    @PostMapping("/aws/createQueue")
+    public Mono<String> postCreateQueue(@RequestBody Map<String, Object> requestBody){
+        return Mono.just(usuarioSQSService.createQueue((String) requestBody.get("queueName")));
     }
 }
